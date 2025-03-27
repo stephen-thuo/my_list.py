93123e09470af20c1e5fe159784dfef682fa827b
stephen-thuo <sthuo95@gmail.com>
2025-03-27 14:48:01 +0300

    my_list.py

---
+++ my_list.py
@@ -0,0 +1,22 @@
+my_list = []
+my_list.append (10)
+my_list.append (20)
+my_list.append (30)
+my_list.append (40)
+print (my_list)
+
+my_list.insert (1, 15)
+print (my_list)
+
+extension_list = [50, 60, 70]
+my_list.extend (extension_list)
+print (my_list)
+
+my_list.pop (7)
+print (my_list)
+
+my_list.sort ()
+print (my_list)
+
+index_of_30 = my_list.index (30)
+print (index_of_30)
